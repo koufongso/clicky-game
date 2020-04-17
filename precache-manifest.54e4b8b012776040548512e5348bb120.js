1cf654e3490143aa8cfbf437aac6f71e972d6aab
Fung <koufongso@gmail.com>
2020-04-16 20:22:22 -0700

    Updates

---
+++ precache-manifest.54e4b8b012776040548512e5348bb120.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3edde0f133bbca6d32f16811a9da732c",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "eafd6ce60a541b184f59",
+    "url": "/clicky-game/static/css/main.9ff22654.chunk.css"
+  },
+  {
+    "revision": "c2c13224b9a5812ce2f6",
+    "url": "/clicky-game/static/js/2.1f6cd406.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.1f6cd406.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eafd6ce60a541b184f59",
+    "url": "/clicky-game/static/js/main.a72aa9fa.chunk.js"
+  },
+  {
+    "revision": "9025a9c5d0326b1623d9",
+    "url": "/clicky-game/static/js/runtime-main.ec6097ba.js"
+  },
+  {
+    "revision": "df56e6b6a7734ca8a68179963dccb7ab",
+    "url": "/clicky-game/static/media/buffalo-head.df56e6b6.png"
+  },
+  {
+    "revision": "4b355aaf4fb8a1914ae8aef6f032c709",
+    "url": "/clicky-game/static/media/camel-head.4b355aaf.png"
+  },
+  {
+    "revision": "7a2edb81ff6494d7319def3ddeb8112a",
+    "url": "/clicky-game/static/media/double-fish.7a2edb81.png"
+  },
+  {
+    "revision": "6ef7ccd544d4f61f9382a80924d4b93a",
+    "url": "/clicky-game/static/media/eagle-head.6ef7ccd5.png"
+  },
+  {
+    "revision": "7513d3bf0639cca9f8bcdb6b6febcafb",
+    "url": "/clicky-game/static/media/eating-pelican.7513d3bf.png"
+  },
+  {
+    "revision": "38543fd02727ef9ba4f05a3fc0ba2eea",
+    "url": "/clicky-game/static/media/elephant-head.38543fd0.png"
+  },
+  {
+    "revision": "7be7fcd081398d232097493a4431488e",
+    "url": "/clicky-game/static/media/kangaroo.7be7fcd0.png"
+  },
+  {
+    "revision": "dffdf4e4d887145365b1d981c27d0950",
+    "url": "/clicky-game/static/media/koala.dffdf4e4.png"
+  },
+  {
+    "revision": "4173d638c5fb003ac3b07f5204b1c7f7",
+    "url": "/clicky-game/static/media/labrador-head.4173d638.png"
+  },
+  {
+    "revision": "5517a7bfa307a917adf67b8b680c095c",
+    "url": "/clicky-game/static/media/snake-spiral.5517a7bf.png"
+  },
+  {
+    "revision": "62bb9a6e1705f8d7e2e02bf46c27c6c7",
+    "url": "/clicky-game/static/media/snake-tongue.62bb9a6e.png"
+  },
+  {
+    "revision": "c929d3d4360b9e7e551e0b90afe728e0",
+    "url": "/clicky-game/static/media/spiked-dragon-head.c929d3d4.png"
+  }
+]);
